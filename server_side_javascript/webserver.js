1c04d144488d10c8a97d429eed6a75617398b6cc
Dev-JeromeBaek <superior051@icloud.com>
2017-10-06 15:49:54 +0900

    .

--- server_side_javascript/webserver.js
+++ server_side_javascript/webserver.js
@@ -1,4 +1,14 @@
+
+
 const http = require('http');
+	// Node.js에서 만든 기본적인 웹서버
+	// require('http'); -> Node js 에서 만든 http라는 모듈을 요구하다.
+	// require함수가 실행되면 http라는 모듈을 가져와 사용할 수 있도록하는것이 이 코드의 핵심.
+	// 그리고, return 값을 http에 담는다.
+	// const = constant의 약자
+	// javascript에서 사용하는 상수
+	// 상수 - 한번 할달이 되면 그다음부턴 그 할당된 값을 바꿀 수 없는 값을 의미한다.
+	// 한번 모듈을 load해오면 바꿀 이유가 없기 때문에 상수처리 해놓는 것임.
 
 const hostname = '127.0.0.1';
 const port = 3333;
@@ -16,5 +26,4 @@ server.listen(port, hostname, () => {
 	// 그 웹서버가 3000번 포트를 listening하게 시키는 코드이다.
 	// 클라이언트가 127.0.0.1로 접속을때 해당 포트를 응답시키게끔 만드는 명령.
   console.log(`Server running at http://${hostname}:${port}/`);
-  console.log("Node.js Server Test GitHub");
 });
