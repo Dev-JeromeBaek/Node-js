63aab154f3d3fdb3d1440e727f4c3acc68a1e2d2
Dev-JeromeBaek <superior051@icloud.com>
2017-10-18 17:37:46 +0900

    Jerome_Core

---
+++ server_side_javascript/app.js
@@ -0,0 +1,21 @@
+var express = require('express');
+var app = express();
+
+// get, post 방식의 호출에 대한 함수
+// get, post 함수에 대한 콜백함수에는 request와 response가 반환된다.
+app.get('/', function(req, res) {
+	res.send('Hello Home Page!');
+});
+
+// get, post함수를 'router'라고 한다. route : 길을 찾는다.
+// get, post함수가 하는 일을 'routing 한다' 라고 한다.
+// 요청에 대한 연결을 해주는 역할.
+app.get('/login', function(req, res) {
+	res.send('<h1>Login Please!</h1>');
+});
+
+// 3000번 포트로의 접속 확인.
+app.listen(3000, function () {
+  console.log('Example app listening on port 3000!');
+  console.log('Connected 3000 port!');
+});
